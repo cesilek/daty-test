68f2c3168f8366308dd93605f0c3df5f68baa79b
Kryštof Česal <cesilek@gmail.com>
2014-06-06 12:11:50 +0200

    fixed autoinclude

--- index.php
+++ index.php
@@ -4,10 +4,13 @@ date_default_timezone_set('Europe/Prague');
 error_reporting(-1);
 
 // autoinclude
-$recIterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(__DIR__ . "/app"));
-foreach (new RegexIterator($recIterator, '/\.php$/i') as $item) {
-	include $item->getPathname();
-}
+spl_autoload_register(function ($class) {
+    $recIterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(__DIR__ . "/app"));
+    $class = call_user_func('end', explode('\\', $class));
+    foreach (new RegexIterator($recIterator, "/$class\.php$/i") as $item) {
+        include_once $item->getPathname();
+    }
+});
 
 $presenter = new \CrawlerModule\StatistickeUdajePresenter();
 if (!empty($_GET['ico'])) {
